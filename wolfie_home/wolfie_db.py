08601bfb55fc1c4b36afa2127c18e01c87e12df5
cjackie <chaojie.keepgoing@gmail.com>
2016-04-27 00:32:35 +0000

    helper class to interact with database

--- wolfie_home/wolfie_db.py
+++ wolfie_home/wolfie_db.py
@@ -6,11 +6,20 @@ class WolfieDB():
     '''
     Careful!! all query statement are not escaped, which is vulenerable
     to attacks.
+    Q:
+    1. under what circumstances, reconnect will fail?
+    one connection can only work with one table? too many reuqests will 
+    get rejected?
     '''
     USERS_TABLE = 'users'
+    HOUSE_TABLE = 'house'
 
     QUERY_USER_INSERT = 0
     QUERY_USER_GET = 1
+
+    QUERY_HOUSE_INSERT = 2
+    QUERY_HOUSE_GET = 3
+    QUERY_HOUSE_GET_ALL = 4
     
     def __init__(self, config=None):
         '''
@@ -33,15 +42,12 @@ class WolfieDB():
         reconnect, then gives up it still can not connect.
         return True on success, False otherwise.
         '''
-        ret = self.conn.is_connected():
-        self.conn.get_rows()
-        if ret == True:
+        if self.conn.is_connected():
             return True
+        self.conn.get_rows()
 
         logging.warning('lose connection, and try to reconnect')
-        ret = self.conn.reconnect(attempts=3, delay=0)
-        self.conn.get_rows()
-        if ret == True:
+        if self.conn.reconnect(attempts=3, delay=100) == True:
             logging.warning('reconnect successfully')
             return True
         else:
@@ -56,52 +62,35 @@ class WolfieDB():
         @cmd: QUERY_USER_INFO_INSERT, insert a new user. QUERY_USER_INFO_GET
             query information about a user
         @columns: type is dictionary. data of each columns. if @cmd is 
-            QUERY_USER_INFO_GET, only uid is needed.
+            QUERY_USER_GET, only username is needed.
         return a tuple: (bool, data). bool indicate if query exceeds. when 
-            QUERY_USER_INFO_GET is used, data returns a dictionary representing
+            QUERY_USER_GET is used, data returns a dictionary representing
             the row.
         '''
         if self.__check_conn() == False:
+            logging.warning('connection failed')
             return False, None
 
-        if cmd == QUERY_USER_INSERT:
-            # checking columns. only simple type checks.
-            if type(columns) != dict or not columns.has_key('username') or \
-               not columns.has_key('email')  or not columns.has_key('wolfie_home') or \
-               not columns.has_key('devices'):
-                logging.error('parameters are not right');
-                raise Exception('parameter error\n')
-            if type(columns['username']) != str or columns['username'] == '' or \
-               type(columns['password']) != str or columns['password'] == '' or \
-               type(columns['email']) != str or columns['email'] == '' or \
-               type(columns['wolfie_home']) != str or columns['wolfie_home'] == '' or \
-               type(columns['devices']) != str or columns['devices'] == '':
-                logging.error('parameters are not right');
-                raise Exception('parameter error\n')
-
+        if cmd == WolfieDB.QUERY_USER_INSERT:
             # build insert statement
-            statement = 'INSERT INTO %s ' % USERS_TABLE + \
-                 '(username, password, email, wolfie_home, devices) ' + \
-                 'VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\')' \
+            statement = 'INSERT INTO %s ' %  WolfieDB.USERS_TABLE + \
+                 '(username, password, email, house_info) ' + \
+                 'VALUES (\'%s\', \'%s\', \'%s\', \'%s\')' \
                  % (columns['username'], columns['password'], columns['email'], \
-                    columns['wolfie_home'], columns['devices'])
+                    columns['house_info'])
             try:
                 self.conn.cmd_query(statement)
                 self.conn.commit()
             except Exception as e:
-                logging.warning('error when trying to insert a row: %s', str(e))
+                logging.warning('error when trying to insert a row to users table: %s', str(e))
                 return False, None
-            logging.info('insert a query')
+            logging.info('insert a data to users table')
             return True, None
 
-        else if cmd == QUERY_USER_GET:
-            # parameter type checking
-            if type(columns) != dict or not columns.has_key('username') or \
-               type(columns['username']) != str:
-                logging.error('parameters are not right');
-                raise Exception('parameter error\n')
+        elif cmd == WolfieDB.QUERY_USER_GET:
             # query
-            statement = 'SELECT * FROM users WHERE username=%s' % (columns['username'])
+            statement = 'SELECT * FROM %s WHERE username=\'%s\'' \
+                        % (WolfieDB.USERS_TABLE, columns['username'])
             self.conn.cmd_query(statement) # this should not throw exception
             (data, _) = self.conn.get_row()
             if data == None:
@@ -111,8 +100,7 @@ class WolfieDB():
             user_info['username'] = str(data[1])
             user_info['password'] = str(data[2])
             user_info['email'] = str(data[3])
-            user_info['wolfie_home'] = str(data[4])
-            user_info['devices'] = json.loads(data[5])
+            user_info['house_info'] = json.loads(str(data[4]))
             return True, user_info
 
         else:
@@ -120,9 +108,94 @@ class WolfieDB():
             raise Exception('invalid commnad\n')
 
 
+    def query_house(self, cmd, columns):
+        '''
+        query related to house table.
+        @cmd, three possible commands, QUERY_HOUSE_INSERT, QUERY_HOUSE_GET
+              QUERY_HOUSE_GET_ALL. QUERY_HOUSE_INSERT insert a new module into 
+              the database. QUERY_HOUSE_GET gets the most recent data about a module.
+              QUERY_HOUSE_GET_ALL get all data about a module.
+        @columns, dict, depending on the cmd.
+        @return, (status, data)
+        '''            
+        if self.__check_conn() == False:
+            logging.warning('connection failed')
+            return False, None
+
+        if cmd == WolfieDB.QUERY_HOUSE_INSERT:
+            '''
+            @columns['house'], house name
+            @columns['uid'], uid id
+            @columns['module'], module type
+            @columns['mod_content'], module type
+            @return (status, data). data is None
+            '''
+            statement = ('INSERT INTO %s (house, uid, module, mod_content) ' + \
+                 'VALUES (\'%s\', \'%s\', \'%s\', \'%s\')')\
+                 % (WolfieDB.HOUSE_TABLE, columns['house'], columns['uid'], \
+                     columns['module'], columns['mod_content'])
+            try:
+                self.conn.cmd_query(statement)
+                self.conn.commit()
+            except Exception as e:
+                logging.warning('error when trying to insert a row to house table: %s', str(e))
+                return False, None
+            logging.info('insert a data to house table')
+            return True, None
+
+        elif cmd == WolfieDB.QUERY_HOUSE_GET:
+            '''
+            get the most recent data about a module
+            @columns['uid'], the identifier of the module
+            @columns['module'], the module type
+            @return, (status, data). status is boolean, and data is a dict. 
+            '''
+            statement = ('SELECT * FROM %s WHERE uid=\'%s\' AND module=\'%s\'' \
+                        + 'ORDER BY time DESC LIMIT 1') \
+                        % (WolfieDB.HOUSE_TABLE, columns['uid'], columns['module'])
+            self.conn.cmd_query(statement) # this should not throw exception
+            (data, _) = self.conn.get_row()
+            if data == None:
+                return False, None
+            module_info = dict()
+            module_info['house'] = str(data[0])
+            module_info['uid'] = str(data[1])
+            module_info['module'] = str(data[2])
+            module_info['mod_content'] = json.loads(str(data[3]))
+            module_info['time'] = str(data[4])
+            return True, module_info
+
+        elif cmd == WolfieDB.QUERY_HOUSE_GET_ALL:
+            '''
+            similar to QUERY_HOUSE_GET. but the data is not recent one of the module,
+            it is all data of that module.
+            '''
+            statement = ('SELECT * FROM %s WHERE uid=\'%s\' AND module=\'%s\'') \
+                        % (WolfieDB.HOUSE_TABLE, columns['uid'], columns['module'])
+            self.conn.cmd_query(statement) # this should not throw exception
+            data, _ = self.conn.get_rows()
+            if data == []:
+                return False, None
+
+            module_info_all = []
+            for i in range(len(data)):
+                module_info = dict()
+                module_info['house'] = str(data[i][0])
+                module_info['uid'] = str(data[i][1])
+                module_info['module'] = str(data[i][2])
+                module_info['mod_content'] = json.loads(str(data[i][3]))
+                module_info['time'] = str(data[i][4])
+                module_info_all.append(module_info)
+            return True, module_info_all
+
+        else:
+            logging.error('invalid command in query_house')
+            raise Exception('invalid commnad.\n')
+
+
+
             
-
-
+            
                 
 
                 
--- wolfie_home/wolfie_db_test.py
+++ wolfie_home/wolfie_db_test.py
@@ -1,1 +1,69 @@
 # testing
+
+import logging
+import json
+from wolfie_db import WolfieDB 
+
+logging.basicConfig(filename='wolfie_db_test.log', \
+                    format='%(levelname)s,%(asctime)s,%(filename)s:%(message)s', \
+                    level=logging.NOTSET)
+
+db = WolfieDB()
+# house_info = '''
+# {
+#     "house_name": "jack_house",
+#     "mod_list": [
+# 	{
+# 	    "uid": "1038592",
+# 	    "module": "wireless",
+# 	    "location": "living room"
+# 	}
+#     ]
+# }
+# '''
+# inserted_data = {
+#     'username': 'jack',
+#     'password': '123456',
+#     'email': '123456wolfie@gmail.com',
+#     'house_info': house_info
+# }
+
+# # QUERY_USER_INSERT
+# (status, _) = db.query_user(WolfieDB.QUERY_USER_INSERT, inserted_data)
+# if status == False:
+#     print "status is False?"
+# else:
+#     print "success"
+
+# # QUERY_USER_GET
+# (status, data) = db.query_user(WolfieDB.QUERY_USER_GET, {'username': 'jack'})
+# if status == False:
+#     print "status is False?"
+# else:
+#     print "success"
+#     print data
+
+# QUERY_HOUSE_INSERT
+mod_content = '''
+{
+    "Btyp": 1,
+    "Bater": 43
+}
+'''
+inserted_data = {
+    'house': 'jack_house',
+    'uid': '12345678',
+    'module': 'battery',
+    'mod_content': mod_content
+}
+r = db.query_house(WolfieDB.QUERY_HOUSE_INSERT, inserted_data)
+print "HOUSE_INSERT returns: %s" % str(r)
+
+# QUERY_HOUSE_GET_ALL
+r = db.query_house(WolfieDB.QUERY_HOUSE_GET_ALL, {'uid': '12345678', 'module': 'battery'})
+print "HOUSE_GET_ALL returns: %s" % str(r)
+
+# QUERY_HOUSE_GET
+r = db.query_house(WolfieDB.QUERY_HOUSE_GET, {'uid': '12345678', 'module': 'battery'})
+print "HOUSE_GET returns: %s" % str(r)
+
